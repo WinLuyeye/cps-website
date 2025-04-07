48411a7dc9f23f2d12932ad11cb1d118183ef51f
Win Luyeye <Winnerluyeye1@gmail.com>
2025-04-07 13:09:55 +0200

    Refact Contact Section

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -8,7 +8,7 @@ const ContactSection = () => {
       {/* Background image */}
       <div className="absolute inset-0 z-0">
         <Image
-          src="/ContactImage.png"  // Remplacez par le chemin de votre image
+          src="/ContactImage.png" // Remplacez par le chemin de votre image
           alt="Contact Background"
           layout="fill"
           objectFit="cover"
@@ -23,15 +23,22 @@ const ContactSection = () => {
       {/* Content */}
       <div className="relative z-10 flex flex-col items-start justify-center h-full max-w-3xl mx-auto px-8 text-white">
         <h2 className="text-4xl font-extrabold leading-tight">
-          Parquez plus rapidement et <br /> 
-          de manière plus sécurisée avec <br />
-          notre solution intelligente de parking.
+          🚗 Parquez plus rapidement et de manière plus sécurisée avec notre
+          solution intelligente de parking
         </h2>
         <p className="mt-4 text-lg text-gray-200">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, 
-          luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          Grâce à FindPark, la recherche de stationnement devient une expérience
+          rapide et sans stress. Notre technologie innovante vous aide à trouver
+          une place disponible en temps réel, vous permettant ainsi de gagner du
+          temps et de vous garer de manière sécurisée. Simplifiez vos
+          déplacements et profitez d&apos;une solution de stationnement conçue
+          pour vous offrir une tranquillité d&apos;esprit, à chaque étape de
+          votre trajet.
         </p>
-        <Link href="/contact" className="mt-6 px-6 py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600">
+        <Link
+          href="/contact"
+          className="mt-6 px-6 py-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600"
+        >
           Contactez-nous
         </Link>
       </div>
